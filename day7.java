dfe0400f2b6eb7249c3d8affaf8de1a90b1a1fa9
Anishi Mishra <anishimishra27@gmail.com>
2020-08-04 09:26:27 -0700

    day 7 of code

---
+++ day7.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        scanner.close();
+        int i;
+        if(n>=1 && n<=1000)
+        {
+            for(i=n-1; i >= 0;i--)
+            {
+                if(arr[i]>=1 && arr[i]<=10000)
+                {
+                    System.out.print(arr[i] + " ");
+                }
+            }
+        }
+    }
+}
